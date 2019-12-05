7c7b56ba72dc149225f476bf934f54ae71a333a5
perrito <ma.ugalde.c@gmail.com>
2019-12-05 06:19:32 -0300

    commit 2

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from.models import Autor, Usuario, Comuna , Region , Pieza, Tipo
+from django.views import generic
 
 #Create your views here.
 def index(request):
@@ -14,7 +15,12 @@ def index(request):
 
 	# mostrar estado
 	# aqui podria ponerse algo relacionado con el stock
-	
+
 	return render( request , 'index.html' , context = { 'num_Autor':num_Autor , 'num_Usuario':num_Usuario ,'num_Pieza':num_Pieza,'num_Comuna':num_Comuna ,'num_Region':num_Region ,'num_Tipo':num_Tipo} ,
         )
 
+class PiezaListView(generic.ListView):
+    model = Pieza
+
+class PiezaDetailView(generic.DetailView):
+ model = Pieza
